0c68b49b523a4b0a0e52748e89b62a537bcc9cb9
Farzeen Zainab <farzeenzaidi51@gmail.com>
2024-08-07 06:32:55 +0500

    working with animation propagation and orchestration

--- src/components/variants.jsx
+++ src/components/variants.jsx
@@ -1,23 +1,32 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 const Variants = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   const variants = {
-    visible: {
-      opacity: 1,
-    },
     hidden: {
       opacity: 0,
+      transition: {
+        when: "afterChildren",
+        staggerChildren: 0.1,
+      },
     },
-    partiallyVisible: {
-      opacity: 0.5,
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.1,
+      },
     },
   };
 
   const item = {
     hidden: {
+      opacity: 0,
       x: 0,
     },
-    visible: { x: 10 },
+    visible: { x: 12, opacity: 1 },
   };
   return (
     <div>
@@ -45,28 +54,49 @@ const Variants = () => {
             its own animate property
         */}
 
-      <motion.ul
-        className="w-[220px] h-auto p-4 bg-white rounded-lg mt-6"
-        initial="hidden"
-        animate="visible" // we can pass variants conditionally as per the state
-        variants={variants}
+      {/* 
+          Orchestration:
+          By default, all these animations will start simultaneosly. But by usings variants
+          we gain extra transition props like when, delayChildren, and staggerChildren that
+          can let parents orchestrate the execution of child animations
+        
+        */}
+
+      <motion.div>
+        <motion.ul
+          initial="hidden"
+          animate={isVisible ? "visible" : "hidden"}
+          variants={variants}
+          className="w-[220px] h-auto p-4 bg-white rounded-lg mt-6"
+        >
+          <motion.li className="mb-2" variants={item}>
+            Menu item 1
+          </motion.li>
+          <motion.li className="mb-2" variants={item}>
+            Menu item 2
+          </motion.li>
+          <motion.li className="mb-2" variants={item}>
+            Menu item 3
+          </motion.li>
+          <motion.li className="mb-2" variants={item}>
+            Menu item 4
+          </motion.li>
+          <motion.li className="mb-2" variants={item}>
+            Menu item 5
+          </motion.li>
+        </motion.ul>
+      </motion.div>
+
+      <button
+        onClick={() =>
+          setIsVisible((prev) => {
+            return !prev;
+          })
+        }
+        className="mt-8"
       >
-        <motion.li className="mb-2" variants={item}>
-          Menu item 1
-        </motion.li>
-        <motion.li className="mb-2" variants={item}>
-          Menu item 2
-        </motion.li>
-        <motion.li className="mb-2" variants={item}>
-          Menu item 3
-        </motion.li>
-        <motion.li className="mb-2" variants={item}>
-          Menu item 4
-        </motion.li>
-        <motion.li className="mb-2" variants={item}>
-          Menu item 5
-        </motion.li>
-      </motion.ul>
+        Toggle
+      </button>
     </div>
   );
 };
